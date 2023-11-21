9f8d3e5779a6c9dd742baeaac770e4ee19e56b5e
Gustavo Henrique <gustavohenriquecns.wk@gmail.com>
2023-11-21 09:42:05 -0800

    VAR INIT´S

--- app/src/main/java/com/gustavohenrique/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/gustavohenrique/alcoolougasolina/MainActivity.kt
@@ -2,10 +2,36 @@ package com.gustavohenrique.alcoolougasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textInputAlcool: TextInputLayout
+    private lateinit var editAlcool: TextInputEditText
+
+    private lateinit var textInputGasolina: TextInputLayout
+    private lateinit var editGasolina: TextInputEditText
+
+    private lateinit var btnCalcular: Button
+    private lateinit var textResultado: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        incializarComponentesInterface()
+    }
+
+    private fun incializarComponentesInterface() {
+        textInputAlcool = findViewById(R.id.text_input_alcool)
+        editAlcool = findViewById(R.id.edit_alcool)
+
+        textInputGasolina = findViewById(R.id.text_input_gasolina)
+        editGasolina = findViewById(R.id.edit_gasolina)
+
+        textResultado = findViewById(R.id.text_resultado)
+        btnCalcular = findViewById(R.id.btn_calcular)
+
     }
 }
